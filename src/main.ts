4ad8464bb3a98d97c6d803aef0cc5a5223526c15
AriefRahmanW <rivurjr@gmail.com>
2022-08-08 06:51:33 +0700

    removing console log

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  app.useGlobalPipes(new ValidationPipe({
-    exceptionFactory: (errors) => {
-      console.log(errors)
-    }
-  }));
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
       .setTitle('Todo App API')
       .setDescription('Todo App description')
